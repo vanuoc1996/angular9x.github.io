0824088341d90a79c973fb2c93602dddaa1d7a02
uoc1234 <70582168+uoc1234@users.noreply.github.com>
2021-06-03 16:25:33 +0700

    uoc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { BodyComponent } from './body/body.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { BodyComponent } from './layouts/body/body.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { ProductComponent } from './product/product.component';
+import { BarComponent } from './core/bar/bar.component';
 
 @NgModule({
   declarations: [
@@ -11,6 +13,8 @@ import { FooterComponent } from './footer/footer.component';
     HeaderComponent,
     BodyComponent,
     FooterComponent,
+    ProductComponent,
+    BarComponent,
    
   ],
   imports: [
--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+ img = "https://suno.vn/blog/wp-content/uploads/2014/12/nike-lich-su-thiet-ke-logo.jpg";
+  constructor() {
+   }
 
   ngOnInit(): void {
   }
